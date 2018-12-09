033517ed03eb1848a16fe8221aa1b714ef689279
Jianyu Zhu <jianyu@typsy.com>
2018-12-09 14:29:40 +1100

    refactor the output

---
+++ dotnet-code-challenge/Args.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_code_challenge
+{
+    public class Args
+    {
+        public bool CaulfieldRace { get; set; }
+
+        public bool WolferhamptonRace { get; set; }
+    }
+}
--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -9,34 +9,68 @@ namespace dotnet_code_challenge
     {
         static void Main(string[] args)
         {
-            var doc = XmlParser<CaulfieldRace>.Parse("FeedData/Caulfield_Race1.xml");
+            var parser = new Fclp.FluentCommandLineParser<Args>();
+            parser.Setup(x => x.CaulfieldRace).As('c', "Caulfield").SetDefault(false);
+            parser.Setup(x => x.WolferhamptonRace).As('w', "Wolferhampton").SetDefault(false);
 
-            foreach (var race in doc.Races.Race)
+            var result = parser.Parse(args);
+
+            if (result.EmptyArgs)
             {
-                Console.WriteLine($"Race {race.Number}:");
-                var horsePrices = race.Prices.Price.Horses.Horse.OrderBy(x => double.Parse(x.Price));
-                var horses = race.Horses.Horse;
-                foreach (var horsePrice in horsePrices)
-                {
-                    var horse = horses.Single(x => x.Number == horsePrice._Number);
-                    Console.WriteLine($"Horse {horse.Number} - {horse.Name} - Price: {horsePrice.Price}");
-                }
+                Console.WriteLine($"Usage: -[c]Caulfield true|false -w Wolferhampton true|false ");
+                Environment.Exit(1);
             }
 
-            var doc2 = JsonParser<WolferhamptonRace>.Parse("FeedData/Wolferhampton_Race1.json");
-
-            foreach (var market in doc2.RawData.Markets)
+            if (parser.Object.CaulfieldRace)
             {
-                Console.WriteLine($"Market {market.Id}:");
-                var selectionPrices = market.Selections.OrderBy(x => x.Price);
-                var participants = doc2.RawData.Participants;
+                Console.WriteLine("======================================================");
+                Console.WriteLine("Caulfield Race Price Order");
+                Console.WriteLine("======================================================");
 
-                foreach (var selectionPrice in selectionPrices)
+                var doc = XmlParser<CaulfieldRace>.Parse("FeedData/Caulfield_Race1.xml");
+
+                foreach (var race in doc.Races.Race)
                 {
-                    var participant = participants.Single(x => x.Id == Int32.Parse(selectionPrice.Tags.participant));
-                    Console.WriteLine($"Participant {participant.Id} - {participant.Name} - Price: {selectionPrice.Price}");
+                    Console.WriteLine($"Race {race.Number}:");
+                    var horsePrices = race.Prices.Price.Horses.Horse.OrderBy(x => double.Parse(x.Price));
+                    var horses = race.Horses.Horse;
+                    foreach (var horsePrice in horsePrices)
+                    {
+                        var horse = horses.Single(x => x.Number == horsePrice._Number);
+                        Console.WriteLine($"Horse {horse.Number} - {horse.Name} - Price: {horsePrice.Price}");
+                    }
                 }
+
+                Console.WriteLine();
             }
+
+
+            if (parser.Object.WolferhamptonRace)
+            {
+                Console.WriteLine("======================================================");
+                Console.WriteLine("Wolferhampton Race Price Order");
+                Console.WriteLine("======================================================");
+
+                var doc = JsonParser<WolferhamptonRace>.Parse("FeedData/Wolferhampton_Race1.json");
+
+                foreach (var market in doc.RawData.Markets)
+                {
+                    Console.WriteLine($"Market {market.Id}:");
+                    var selectionPrices = market.Selections.OrderBy(x => x.Price);
+                    var participants = doc.RawData.Participants;
+
+                    foreach (var selectionPrice in selectionPrices)
+                    {
+                        var participant = participants.Single(x => x.Id == Int32.Parse(selectionPrice.Tags.participant));
+                        Console.WriteLine($"Participant {participant.Id} - {participant.Name} - Price: {selectionPrice.Price}");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Press any key to exit . . .");
+            Console.ReadKey();
         }
     }
 }
